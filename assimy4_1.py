a290b852e7fe6a3b6f5f95fce218c6d385b64d7c
Misha <msokol060825@mail.ru>
2023-08-20 20:50:06 +0300

    initial

---
+++ assimy4_1.py
@@ -0,0 +1,35 @@
+# N**2
+def strcounter(s):
+    for sym in s:
+        counter = 0 
+        for sub_sym in s:
+            if sym == sub_sym:
+                counter +=1
+        print(sym, counter)
+
+strcounter('aaaabbbbbccccccddd')
+
+
+# N*M
+
+def skrcounter(s):
+    for sym in set(s):
+        counter =0
+        for sub_sym in s:
+            if sym ==sub_sym:
+                counter += 1
+        print (sym, counter)
+
+skrcounter('aaaabbbcccdddd')
+
+#N
+
+def strcounter_2(s):
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym,0)+1
+
+    for sym, count in syms_counter.items():
+        print(sym, count)
+
+strcounter_2('aaabbbbbccccd')
